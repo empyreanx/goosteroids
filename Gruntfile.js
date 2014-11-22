c2a6801b8e2cfd096ee56f989268783ca3db4207
James McLean <james@mcleanlabs.ca>
2014-11-22 16:18:28 -0500

    Created project files.

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+module.exports = function(grunt) {
+	
+  grunt.initConfig({
+    uglify: {
+      prod: {
+        files: {
+          'web/js/app.js': 'tmp/app.js'
+        }
+      }
+    },
+
+    browserify: {
+      dev: {
+        files: {
+          'web/js/app.js': ['src/index.js']
+        },
+        options: {
+          debug: true
+        }
+      },
+      prod: {
+        files: {
+          'tmp/app.js': ['src/index.js']
+        },
+        options: {}
+      }
+    },
+
+    watch: {
+      dev: {
+        files: [
+          './src/**/*.js'
+        ],
+        tasks: [
+          'browserify:dev'
+        ],
+        options: {
+          livereload: true,
+          spawn: false,
+          atBegin: true
+        }
+      }
+    },
+
+    connect: {
+      dev: {
+        options: {
+          port: 3000,
+          base: 'web/'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('debug', [
+    'browserify:dev'
+  ]);
+
+  grunt.registerTask('server', [
+    'connect:dev',
+    'watch:dev'
+  ]);
+
+  grunt.registerTask('release', [
+    'browserify:prod',
+    'uglify:prod'
+  ]);
+
+  grunt.registerTask('default', [
+    'debug'
+  ]);
+};
---
+++ src/index.js
@@ -0,0 +1,4 @@
+var Base = require('basejs');
+var d = document.createElement('DIV');
+d.textContent = 'Hello world!';
+document.body.appendChild(d);
